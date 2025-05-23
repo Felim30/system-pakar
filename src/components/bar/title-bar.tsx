ec1b667ab491c6776985393c9c9002dd19e3a139
Felim301203 <134253832+Felim301203@users.noreply.github.com>
2025-05-23 16:15:17 +0700

    Update title-bar.tsx

--- src/components/bar/title-bar.tsx
+++ src/components/bar/title-bar.tsx
@@ -12,7 +12,7 @@ export const TitleBar : React.FC<{title : string}> = ({title}) => {
     
     return (
         <div className='font-bold p-8 w-full text-2xl text-center fixed bg-white flex items-center justify-start gap-2'>
-            <IonIcon icon={arrowBack} onClick={handleDashboard} className='w-1/5'/>
+            <IonIcon icon={arrowBack} onClick={handleDashboard} className='w-1/5 text-black'/>
             <p className='text-2xl text-center text-black w-3/5'>{title}</p>
         </div>
     )
